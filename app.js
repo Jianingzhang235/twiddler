fe9158e54c4d323ad0e67e2b8f26edc6686d64d5
Jianingzhang235 <jianing_chang@hotmail.com>
2019-03-19 08:40:17 -0700

    add user buttons

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ $(document).ready(function(){
     var index = 0 ;
     while(index < streams.home.length) {
       let tweet = streams.home[index];
+      let timestamp = streams.home[index].created_at;
+      
       const $tweet = $('<div></div>');
-            $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      const $user1 =$(`<button class='ti'>${tweet.user}</button>`);
+            $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' +  timestamp);
+            $user1.prependTo($maindiv)
             $tweet.prependTo($maindiv);
           index ++;
       }
@@ -16,3 +20,16 @@ $(document).ready(function(){
   });
 
 });
+   // const $tweet = $('<div></div>');
+    //       let button = $('<button></button>'); 
+    //         button.text(${tweet.user}) 
+    //         $tweet.text(tweet.user + ': ' + tweet.message + ' ' +  timestamp);
+    //         button.prependTo($maindiv)
+    //         $tweet.prependTo($maindiv);
+    //       index ++;
+
+      //     const $tweet = $('<div></div>');
+      // const $user1 =$('<div><span>${tweet.user}</span><div>');
+      //       $tweet.text('@' + $user1 + ': ' + tweet.message + ' ' +  timestamp);
+      //       $tweet.prependTo($maindiv);
+      //     index ++;
